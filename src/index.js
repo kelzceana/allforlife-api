738abd1253b0ec31ef071fa6ae5aa0d17c7b06f3
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-02-02 06:49:09 -0500

    added body-parser module and established ther user route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const db = require("./db/dbIndex");
+const bodyParser = require('body-parser');
+
+// routes constants
+const users = require("./routes/users");
 
 
 //initializing middleware
-app.use(express.json({extended:false}));
 
-app.use("/api/users", require("./routes/users"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//routes
+app.use("/api/users", users(db));
+
+
 
 //app listening
 app.listen(PORT, ()=>{
