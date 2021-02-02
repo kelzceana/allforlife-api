00d967ce730fcd1c8d2b8ef8dab29615d094c70d
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-02-02 06:50:23 -0500

    added queries to get all exisiting users from the customer table

--- src/db/dbIndex.js
+++ src/db/dbIndex.js
@@ -1,7 +1,11 @@
 const pg = require('pg');
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
+  user:"development",
+  password: "development",
+  database: "allforlife",
+  host: "localhost",
+  port: 5432
 });
 
 client.connect().catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
